9bd54c98c3930da33b7009b6478c64b0cc25dd18
Sharmin Akter <sharminakter0411@gmail.com>
2026-07-12 00:02:53 +0600

    admin api completed

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -2,44 +2,59 @@ import type { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import http from "http-status";
+import { adminService } from "./admin.service";
 
 const getAllUsers = catchAsync( async(req: Request, res: Response)=>{
+
+    const {users, totalUsers} = await adminService.getAllUsers();
 
     sendResponse(res, {
         success: true,
         statusCode: http.OK,
         message: "Users Retrieved Successfully.",
-        data: {}
+        data: users,
+        meta: {
+            total: totalUsers
+        }
     });
 });
 
 const updateUserStatus = catchAsync( async(req: Request, res: Response)=>{
 
+    const userId = req.params.id;
+    const {status} = req.body;
+
+    const result = await adminService.updateUserStatus(userId as string, status);
+
     sendResponse(res, {
         success: true,
         statusCode: http.OK,
         message: "Users Status Updated Successfully.",
-        data: {}
+        data: result
     });
 });
 
 const getAllProperties = catchAsync( async(req: Request, res: Response)=>{
 
+    const result = await adminService.getAllProperties();
+
     sendResponse(res, {
         success: true,
         statusCode: http.OK,
         message: "Properties Retrieved Successfully.",
-        data: {}
+        data: result
     });
 });
 
 const getAllRentals = catchAsync( async(req: Request, res: Response)=>{
 
+    const result = await adminService.getAllRentals();
+
     sendResponse(res, {
         success: true,
         statusCode: http.OK,
-        message: "Users Retrieved Successfully.",
-        data: {}
+        message: "Rentals Retrieved Successfully.",
+        data: result
     });
 });
 
--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -1,10 +1,47 @@
-const getAllUsers = async()=>{};
+import type { UserStatus } from "../../../generated/prisma/enums";
+import { prisma } from "../../lib/prisma";
 
-const updateUserStatus = async()=>{};
+const getAllUsers = async()=>{
+    const users = await prisma.user.findMany();
 
-const getAllProperties = async()=>{};
+    const totalUsers = await prisma.user.count()
 
-const getAllRentals = async()=>{};
+    return {users, totalUsers}
+};
+
+const updateUserStatus = async(userId: string, status: UserStatus)=>{
+
+    const user = await prisma.user.findUnique({
+        where:{id: userId}
+    });
+
+    if(!user){
+        throw new Error("User Does Not Exists.");
+    }
+
+    const updatedUser = await prisma.user.update({
+        where: {id: userId},
+        data: {
+            status
+        }
+    });
+
+    return updatedUser
+};
+
+const getAllProperties = async()=>{
+    
+    const properties = await prisma.property.findMany();
+
+    return properties;
+};
+
+const getAllRentals = async()=>{
+
+    const rentals = await prisma.rentalRequest.findMany();
+
+    return rentals
+};
 
 export const adminService = {
     getAllUsers,
